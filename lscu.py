e1db410fb97be44aa01019359696e78958d3133f
TomIO <tomjo00@web.de>
2022-07-04 18:40:06 +0200

    Added <lscu.py> utility script for modifying $LS_COLORS environment variable.

---
+++ lscu.py
@@ -0,0 +1,46 @@
+# ls color utility - 'lscu' v1.0.0
+# Written by @TomIO#2000
+
+import os
+import re
+
+
+def main():
+    def ls_parse():
+        try:
+            env = os.environ.get('LS_COLORS')  # Read the $LS_COLORS environment variable.
+            print(f"Read <$LS_COLORS>")
+            colorize(env)
+        except TypeError or NameError:
+            env = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.swp=00;90:*.tmp=00;90:*.dpkg-dist=00;90:*.dpkg-old=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:*.ucf-old=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:"
+            print(f"Something went wrong while trying to parse <$LS_COLORS> using fallback.")
+            colorize(env)
+        return
+
+    def colorize(env_ls_cols):
+        ls_cols = re.findall(r"[^:]+", env_ls_cols)  # return a list of all indices, separator ':'
+
+        def extract(index, n):  # extract color code
+            try:
+                return "".join(map(str, re.findall(r"(?<==)[^']+", index[n])))  # return a string
+            except IndexError:
+                return None  # return none if the end of the list has been reached
+
+        for i in range(len(ls_cols)):  # for every index...
+            col = extract(ls_cols, i)  # call the solver function
+            print(f"\x1b[{col}m{ls_cols[i]}\x1b[0m:" + "\n" * (extract(ls_cols, i) != extract(ls_cols, i + 1)), end="")
+            # print the proper color via ANSI escape sequence
+            # print the index and reset the coloring
+            # call the solver function, if the current and next color do not match, insert a new line
+
+    def cleanup():  # Exception handling, todo
+        print(f"Script interrupted, quitting")
+
+    try:
+        ls_parse()
+    except KeyboardInterrupt:
+        cleanup()
+
+
+if __name__ == '__main__':
+    main()
