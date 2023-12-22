85a45532dec6fe2b1ff61772baa8607fcf701f54
TomIO <tomjo00@web.de>
2023-12-22 16:29:14 +0100

    Update local file sync excludes.

--- .config/meta/backup_dotfiles.sh
+++ .config/meta/backup_dotfiles.sh
@@ -4,7 +4,7 @@
 # rsync -i --progress -a ~/git/.dotfiles "/run/media/tom/JOSH 32GB/VScode/git/"
 #
 # update in repo
-# rsync --progress --existing --filter=". ${XDG_CONFIG_HOME}/meta/rsync_ignore" --delete-during --out-format='%B %o %n %L' -a "${HOME}/" "${HOME}/git/.dotfiles/"
+# rsync --progress --existing --filter=". ${DOT_FILES}/.config/meta/rsync_ignore" --delete-during -i -a "${HOME}/" "${DOT_FILES}/"
 # deploy from repo
 
 # find . ! -wholename '*.git*' -printf '%04m %P\n'
