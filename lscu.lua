877c4e4bb83438e32b750c37a98761c83e35bcfc
TomIO <tomjo00@web.de>
2024-01-25 12:26:11 +0100

    Reimplement `lscu` in Lua.

--- lscu.lua
+++ lscu.lua
@@ -1,12 +1,17 @@
 #!/usr/bin/env lua
 
-local ls_colors = os.getenv('LS_COLORS')
+---@type string
+local ls_colors = os.getenv('LS_COLORS') or ''
 
----comment
----@param colors any
----@return any
+---@param colors string | number
 local function ls_parse(colors)
-
-  return colors
+  for word in string.gmatch(colors, '[^:]+') do
+    local match, _ = word:gsub('^.+=','')
+    io.write('\x1b[' .. match .. 'm' .. word .. '\x1b[m:')
+  end
+  print()
+  -- return colors
 end
+
+ls_parse(ls_colors)
 
