34ba9ef0d309921614e063eb1f1557c179e66748
TomIO <tomjo00@web.de>
2024-02-09 16:23:53 +0100

    `trilerp.lua` is now working and better than ever. Also added a stylua.toml for this subproject.

--- .local/share/zsh/scripts/col_gen/lua/trilerp.lua
+++ .local/share/zsh/scripts/col_gen/lua/trilerp.lua
@@ -1,36 +1,35 @@
 #!/usr/bin/env luajit
 -- trilerp.lua
 
----@param colors table<string,rgb>[]
+---@param colors rgb[] # named or numbered RGB triplets
+---@return verts       # RGB triplets for min, max and difference between them
 local function find_verts(colors)
+  -- stylua: ignore
   ---@type { min: rgb, max: rgb, r: integer, g: integer, b: integer }
-
   local diffs = {
     min = { r = 255, g = 255, b = 255 },
-    max = { r = 0, g = 0, b = 0 },
-    r = 0,
-    g = 0,
-    b = 0
+    max = { r = 0  , g = 0  , b = 0 },
+    r = 0, g = 0, b = 0,
   }
 
   local min, max = diffs.min, diffs.max
-  for i = 1, #colors do
+  for k, _ in pairs(colors) do
     -- get minimum values
-    min.r = min < colors[i][2] and min.r or colors[i][2]
-    min.g = min < colors[i][2] and min.g or colors[i][2]
-    min.b = min < colors[i][2] and min.b or colors[i][2]
+    min.r = min.r < colors[k].r and min.r or colors[k].r
+    min.g = min.g < colors[k].g and min.g or colors[k].g
+    min.b = min.b < colors[k].b and min.b or colors[k].b
 
     -- get maximum values
-    max.r = max > colors[i][2] and max.r or colors[i][2]
-    max.g = max > colors[i][2] and max.g or colors[i][2]
-    max.b = max > colors[i][2] and max.b or colors[i][2]
+    max.r = max.r > colors[k].r and max.r or colors[k].r
+    max.g = max.g > colors[k].g and max.g or colors[k].g
+    max.b = max.b > colors[k].b and max.b or colors[k].b
   end
   diffs.r = diffs.max.r - diffs.min.r
   diffs.g = diffs.max.g - diffs.min.g
   diffs.b = diffs.max.b - diffs.min.b
 
-  ---@enum verts
-  local verts = { -- vertices
+  ---@alias verts { ['rgb'|'rgB'|'rGb'|'rGB'|'Rgb'|'RgB'|'RGb'|'RGB']: rgb }
+  return { -- vertices
     ['rgb'] = { r = diffs.min.r, g = diffs.min.g, b = diffs.min.b },
     ['rgB'] = { r = diffs.min.r, g = diffs.min.g, b = diffs.max.b },
     ['rGb'] = { r = diffs.min.r, g = diffs.max.g, b = diffs.min.b },
@@ -40,24 +39,30 @@ local function find_verts(colors)
     ['RGb'] = { r = diffs.max.r, g = diffs.max.g, b = diffs.min.b },
     ['RGB'] = { r = diffs.max.r, g = diffs.max.g, b = diffs.max.b },
   }
-
-  return verts
 end
 
 local M = {} -- return table
 
 ---@alias rgb {r: integer, g: integer, b: integer}
----@param hex (string|number)?
----@return rgb
----@alias hex2rgb fun(hex): rgb
+---@alias hex2rgb fun(hex): rgb|rgb[]
+---@param hex string|string[] # One or more Hex colors
+---@return rgb|rgb[] triplet  # One or more RGB triplets
 function M.hex2rgb(hex)
+  -- recurse if we've been handed a table
+  if type(hex) == 'table' then
+    local ret = {}
+    for k, v in pairs(hex) do
+      ret[k] = M.hex2rgb(v)
+    end
+    return ret
+  end
   assert(hex ~= nil, "Can't convert a nil value to RGB")
   hex = hex:gsub('^#', '')
   if #hex == 3 then
     local r, g, b = hex:match('(.)(.)(.)')
     hex = r:rep(2) .. g:rep(2) .. b:rep(2)
   end
-  assert(#hex:match('%x*') == 6, "invalid hex: " .. hex)
+  assert(#hex:match('%x*') == 6, 'invalid hex: ' .. hex)
   local r, g, b = hex:match('(..)(..)(..)')
   return {
     r = tonumber('0x' .. r),
@@ -66,62 +71,56 @@ function M.hex2rgb(hex)
   }
 end
 
---
--- local from = M.hex2rgb(arg[2] or '#191FED')
--- local to   = M.hex2rgb(arg[4] or '#F3D05B')
---
--- ---Find the vertices for the corresponding set of dimensions
--- ---@return { [string]: rgb } verts
--- ---@alias lerp fun(_from, _to, float): rgb
--- ---@param lerp rgb[]    # *2 RGB tuple*
--- ---@param step float  # *Step*
--- ---@return rgb
--- function M.rgb_lerp(lerp, step)
---   return {
---     r = lerp[1].r * (1 - step) + lerp[2].r * step,
---     g = lerp[1].g * (1 - step) + lerp[2].g * step,
---     b = lerp[1].b * (1 - step) + lerp[2].b * step,
---   }
--- end
---
--- ---@alias bilerp table<lerp, lerp, float, float>
--- ---@param colors (string|rgb)[]
--- ---@param x_step float          # x-axis correction factor
--- ---@param y_step float          # y-axis correction factor
--- ---@return rgb                  # {r, g, b}:int
--- function M.rgb_bilerp(colors, x_step, y_step)
---   local v = type(colors[1]) == 'table' and colors or find_verts(colors, 2)
---   return M.rgb_lerp({
---     M.rgb_lerp({ v[1], v[2] }, x_step),
---     M.rgb_lerp({ v[3], v[4] }, x_step),
---   }, y_step
---   )
--- end
---
--- ---@alias trilerp table<bilerp, bilerp, float, float, float>
--- ---@param colors (string|rgb)[]
--- ---@param x_step float # x-axis correction factor
--- ---@param y_step float # y-axis correction factors
--- ---@param z_step float # z-axis correction factors
--- ---@return rgb         # {r, g, b}: int
--- function M.rgb_trilerp(colors, x_step, y_step, z_step)
---   local v = type(colors[1]) == 'table' and colors or find_verts(colors, 3)
---   return M.rgb_lerp({
---     M.rgb_bilerp({ v[1], v[2], v[3], v[4] }, x_step, y_step),
---     M.rgb_bilerp({ v[5], v[6], v[7], v[8] }, x_step, y_step),
---   }, z_step
---   )
--- end
---
--- local x, y, z = 0.5, 0.5, 0.5 -- step values for each axis, might add per axis skew correction later
--- local result = M.rgb_trilerp({
---   verts.rgb, verts.rgB, verts.rGb, verts.rGB,
---   verts.Rgb, verts.RgB, verts.RGb, verts.RGB,
--- }, x, y, z
--- )
---
--- local out_fmt = string.format('%s;%s;%s', math.floor(result.r), math.floor(result.g), math.floor(result.b))
--- local out_str = string.format('#%X%X%X', math.floor(result.r), math.floor(result.g), math.floor(result.b))
--- -- io.write(string.format('\x1b[48;2;%sm%s\x1b[m\n', out_fmt, out_str))
---
+---Find the vertices for the corresponding set of dimensions
+---@alias lerp fun( lerp:{from:rgb, to:rgb}, step:float): rgb
+---@param lerp rgb[] # *2 RGB tuple*
+---@param step float # *Step*
+---@return rgb
+function M.rgb_lerp(lerp, step)
+  return {
+    r = lerp[1].r * (1 - step) + lerp[2].r * step,
+    g = lerp[1].g * (1 - step) + lerp[2].g * step,
+    b = lerp[1].b * (1 - step) + lerp[2].b * step,
+  }
+end
+
+---@alias bilerp table<lerp, lerp, float, float>
+---@param colors rgb[]
+---@param x_step float # x-axis correction factor
+---@param y_step float # y-axis correction factor
+---@return rgb
+function M.rgb_bilerp(colors, x_step, y_step)
+  local v = type(colors[1]) == 'table' and colors or find_verts(colors)
+  return M.rgb_lerp({
+    M.rgb_lerp({ v[1], v[2] }, x_step),
+    M.rgb_lerp({ v[3], v[4] }, x_step),
+  }, y_step)
+end
+
+---@alias trilerp table<bilerp, bilerp, float, float, float>
+---@param colors rgb[]
+---@param x_step float # x-axis correction factor
+---@param y_step float # y-axis correction factors
+---@param z_step float # z-axis correction factors
+---@return rgb
+function M.rgb_trilerp(colors, x_step, y_step, z_step)
+  local v = type(colors[1]) == 'table' and colors or find_verts(colors)
+  return M.rgb_lerp({
+    M.rgb_bilerp({ v[1], v[2], v[3], v[4] }, x_step, y_step),
+    M.rgb_bilerp({ v[5], v[6], v[7], v[8] }, x_step, y_step),
+  }, z_step)
+end
+
+local x, y, z = 0.5, 0.5, 0.5 -- step values for each axis, might add per axis skew correction later
+local verts = find_verts(M.hex2rgb({ arg[1], arg[2] }))
+-- stylua: ignore
+local result = M.rgb_trilerp({
+  verts.rgb, verts.rgB, verts.rGb, verts.rGB,
+  verts.Rgb, verts.RgB, verts.RGb, verts.RGB,
+}, x, y, z)
+
+local out_fmt = string.format('%s;%s;%s', math.floor(result.r), math.floor(result.g), math.floor(result.b))
+local out_str = string.format('#%X%X%X', math.floor(result.r), math.floor(result.g), math.floor(result.b))
+io.write(string.format('\x1b[48;2;%sm%s\x1b[m\n', out_fmt, out_str))
+
 return M
