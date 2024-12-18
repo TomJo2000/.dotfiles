e5f299c70ba7290a71d81f9dc27cb017500da2c1
TomIO <tomjo00@web.de>
2024-12-18 15:35:20 +0100

    git(pr-fetch): handle branch name collisions

--- .config/git/pr-fetch.sh
+++ .config/git/pr-fetch.sh
@@ -6,14 +6,13 @@
 }
 
 function fetch-pr() {
-    local pr="$1" merged state origin_url origin_branch
+    local pr="$1" merged state pr_origin_url pr_branch local_branch branch
 
-    read -rd' ' merged state origin_url origin_branch < <(curl -sL \
+    read -rd' ' merged state pr_origin_url pr_branch < <(curl -sL \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         -H "Accept: application/vnd.github+json" \
         "https://api.github.com/repos/${upstream}/pulls/${pr}" \
-        | jq -r '.merged, .state, .head.repo.ssh_url, .head.ref'
-    )
+        | jq -r '.merged, .state, .head.repo.ssh_url, .head.ref')
 
     # Is the PR already merged?
     [[ "$merged" == 'true' ]] && {
@@ -31,26 +30,38 @@ function fetch-pr() {
         "\e[32mPR ${pr}\e[m: unmerged and open."
 
     # Is the PR's origin remote set up?
-    if [[ "$origin_url" != "$(git config --get "remote.pr${pr}.url")" ]]; then
+    if [[ "$pr_origin_url" != "$(git remote get-url "pr${pr}" 2> /dev/null)" ]]; then
         printf '%b\n' "Setting up remote: '\e[32mpr${pr}\e[m'"
         # Add the "pr${pr}" remote, tracking the PR branch
-        git remote add -t "${origin_branch}" "pr${pr}" "${origin_url}"
+        git remote add -t "${pr_branch}" "pr${pr}" "${pr_origin_url}"
     else
         printf '%b\n' "Remote alread exists: '\e[32mpr${pr}\e[m'"
     fi
     printf '%b\n' \
-        "Fork URL : \e[33m${origin_url}\e[m" \
-        "PR branch: \e[34m${origin_branch}\e[m"
+        "Fork URL : \e[33m${pr_origin_url}\e[m" \
+        "PR branch: \e[34m${pr_branch}\e[m"
+
+    # Check for branch name conflicts
+    local_branch="${pr_branch}"
+    for branch in "$(git rev-parse --git-dir)/refs/heads/"*; do
+        [[ "${branch##*/}" == "${pr_branch}" ]] && {
+            printf '%b\n' \
+                "\e[1;31mBranch name collision detected.\e[m" \
+                "Renaming : \e[34m${pr_branch}\e[m -> \e[32m${pr}-${pr_branch}\e[m"
+            local_branch="${pr}-${pr_branch}"
+            break
+        }
+    done
 
     # Is the PR fetched to a local branch yet?
-    git show-ref --quiet "refs/heads/${origin_branch}" || {
-        git fetch "pr${pr}" "refs/heads/${origin_branch}:${origin_branch}"
-        git branch "${origin_branch}" --set-upstream-to="pr${pr}/${origin_branch}"
+    git show-ref --quiet "refs/heads/${local_branch}" || {
+        git fetch "pr${pr}" "refs/heads/${pr_branch}:${local_branch}" -q
+        git branch "${local_branch}" --set-upstream-to="pr${pr}/${pr_branch}"
     }
 
     printf '%b' "Tracking : "
-    git for-each-ref "refs/heads/${origin_branch}" \
-    --format="%1b[32m${origin_branch}%1b[m...%1b[31m%(upstream:short)%1b[m%0a"
+    git for-each-ref "refs/heads/${local_branch}" \
+    --format="%1b[32m${local_branch}%1b[m...%1b[31m%(upstream:short)%1b[m%0a"
 }
 
 function gh_api_limits() {
@@ -74,7 +85,9 @@ function gh_api_limits() {
 
 # do we have an upstream remote?
 git remote get-url upstream &> /dev/null || {
-    printf '%s\n' "error: No such remote 'upstream'"
+    printf '%b\n' \
+        "error: No such remote 'upstream'" \
+        "\e[33mgit remote add upstream <URL>\e[m"
     exit 2
 }
 
